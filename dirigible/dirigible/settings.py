2ca5437fa92ad68c8fbdc6b87f943ac2b13461da
Pushkar Paranjpe <pushkar@locus.sh>
2016-09-17 16:31:22 +0530

    Added django.contrib.sites to installed apps, moved req.txt into the project folder

--- dirigible/dirigible/settings.py
+++ dirigible/dirigible/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
     'registration',
     'user',
     'feedback',
@@ -65,8 +67,12 @@ WSGI_APPLICATION = 'dirigible.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
